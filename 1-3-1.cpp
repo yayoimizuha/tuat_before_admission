dab49475f861a7e6f4ec3488b78bf97a5f30e30f
yayoimizuha <poko.peko.tomo@icloud.com>
2022-12-17 10:40:14 +0900

    comp automaton

--- 1-3-1.cpp
+++ 1-3-1.cpp
@@ -3,13 +3,12 @@
 #include <cctype>
 #include <string>
 #include <vector>
-#include <tuple>
 
 using namespace std;
 
-vector<size_t> search(const string &text, const string &key);
+void search(const string &text, const string &key);
 
-signed main1(int argc, char *argv[]) {
+signed main(int argc, char *argv[]) {
     if (argc != 2) {
         cout << "usage: " << argv[0] << " input_file" << endl;
         return EXIT_FAILURE;
@@ -29,47 +28,53 @@ signed main1(int argc, char *argv[]) {
         string input;
         cout << "please input search text: " << flush;
         cin >> input;
+        if (input == "q")exit(0);
         for (auto &item: input) {
             item = static_cast<char>(toupper(item));
-            if (string("ATGC").find(item) == string::npos) {
+            if (string("ATGC*").find(item) == string::npos) {
                 cout << "search text error" << endl;
                 //break;
                 goto LOOP_START;
             }
         }
         cout << input << endl;
-        system("pause");
+        search(DNA, input);
     }
 #pragma clang diagnostic pop
 }
 
-vector<size_t> automaton(string text, string key, size_t i);
-
-int main() {
-    automaton("asdjkasgsagfjkadsfhasjf", "asd", 1);
-}
-
-vector<size_t> automaton(string text, string key, size_t i) {
-    cout << text << endl;
-    cout << key << endl;
-    if (key.empty() || text.empty()) {
+vector<string> automaton(string text, string key, string i, u_short incremental) {
+    vector<string> ret, res;
+    if (key.empty() && text.empty())return {i};
+    if (key.empty()) return {i};
+    if (text.empty() && key == "*") {
         return {i};
+    } else if (text.empty())return {""};
+    if (key.at(0) == text.at(0)) {
+        ret = automaton(text.substr(1), key.substr(1), i + text.at(0), 0);
+        for (auto &item: ret) {
+            if (item.empty()) item.erase();
+        }
+        return ret;
     }
     if (key.starts_with('*')) {
-        for (int j = 1; j < text.size(); ++j) {
-            //res= automaton(text.substr(j), key.substr(1), i + 1);
+        for (int j = incremental; j <= text.size(); ++j) {
+            incremental = 0;
+            if (j == 0)incremental = 1;
+            res = automaton(text.substr(j), key.substr(1), i + text.substr(0, j), incremental);
+            ret.insert(ret.end(), res.begin(), res.end());
         }
-    } else if (key[0] == text[0]) {
-        automaton(text.substr(1), key.substr(1), i + 1);
+        return ret;
     }
-    return {1};
+    return {""};
 }
 
-vector<size_t> search(const string &text, const string &key) {
-    size_t index = 0;
-    for (int i = 0; i < text.size() - key.size(); ++i) {
-        for (int j = i; j < i + key.size(); ++j) {
-
+void search(const string &text, const string &key) {
+    for (int i = 0; i < text.size(); ++i) {
+        auto a = automaton(text.substr(i), key, "", 0);
+        for (const auto &t: a) {
+            if (!t.empty())
+                cout << i << '~' << i + t.size() - 1 << '\t' << t << endl;
         }
     }
 }
